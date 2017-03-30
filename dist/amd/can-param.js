3cf37683561c7afba0575bf68b88f2923ce7f3f3
Chasen Le Hara <chasen@chasenlehara.com>
2017-03-29 22:56:53 -0700

    1.0.1

---
+++ dist/amd/can-param.js
@@ -0,0 +1,26 @@
+/*can-param@1.0.0#can-param*/
+define(function (require, exports, module) {
+    var namespace = require('can-namespace');
+    function buildParam(prefix, obj, add) {
+        if (Array.isArray(obj)) {
+            for (var i = 0, l = obj.length; i < l; ++i) {
+                add(prefix + '[]', obj[i]);
+            }
+        } else if (obj && typeof obj === 'object') {
+            for (var name in obj) {
+                buildParam(prefix + '[' + name + ']', obj[name], add);
+            }
+        } else {
+            add(prefix, obj);
+        }
+    }
+    module.exports = namespace.param = function param(object) {
+        var pairs = [], add = function (key, value) {
+                pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+            };
+        for (var name in object) {
+            buildParam(name, object[name], add);
+        }
+        return pairs.join('&').replace(/%20/g, '+');
+    };
+});
---
+++ dist/global/can-param.js
@@ -0,0 +1,141 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod){
+		if(!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, "default": true };
+		for(var p in mod) {
+			if(!esProps[p]) return false;
+		}
+		return true;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			if(useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-param@1.0.0#can-param*/
+define('can-param', function (require, exports, module) {
+    var namespace = require('can-namespace');
+    function buildParam(prefix, obj, add) {
+        if (Array.isArray(obj)) {
+            for (var i = 0, l = obj.length; i < l; ++i) {
+                add(prefix + '[]', obj[i]);
+            }
+        } else if (obj && typeof obj === 'object') {
+            for (var name in obj) {
+                buildParam(prefix + '[' + name + ']', obj[name], add);
+            }
+        } else {
+            add(prefix, obj);
+        }
+    }
+    module.exports = namespace.param = function param(object) {
+        var pairs = [], add = function (key, value) {
+                pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+            };
+        for (var name in object) {
+            buildParam(name, object[name], add);
+        }
+        return pairs.join('&').replace(/%20/g, '+');
+    };
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
