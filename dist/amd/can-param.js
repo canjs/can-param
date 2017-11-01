ad40a22ec10a7b69316dbac4e92a7af18632ae59
imasustink <austin@austinkurpuis.com>
2017-11-01 09:20:29 -0700

    1.0.2

---
+++ dist/amd/can-param.js
@@ -0,0 +1,31 @@
+/*can-param@1.0.1#can-param*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-namespace'
+], function (require, exports, module) {
+    var namespace = require('can-namespace');
+    function buildParam(prefix, obj, add) {
+        if (Array.isArray(obj)) {
+            for (var i = 0, l = obj.length; i < l; ++i) {
+                add(prefix + '[]', obj[i]);
+            }
+        } else if (obj && typeof obj === 'object') {
+            for (var name in obj) {
+                buildParam(prefix + '[' + name + ']', obj[name], add);
+            }
+        } else {
+            add(prefix, obj);
+        }
+    }
+    module.exports = namespace.param = function param(object) {
+        var pairs = [], add = function (key, value) {
+                pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+            };
+        for (var name in object) {
+            buildParam(name, object[name], add);
+        }
+        return pairs.join('&').replace(/%20/g, '+');
+    };
+});
